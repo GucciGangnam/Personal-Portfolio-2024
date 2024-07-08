e69b66c63c719fbe344657784f03a87dc91465c6
Gucci Gangnam <diviner02hive@icloud.com>
2024-07-08 22:24:54 +0700

    NavBar built

--- Personal-Portfolio-2024/src/App.jsx
+++ Personal-Portfolio-2024/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// IMPORTS  
+// React 
+import { useState, useEffect } from 'react';
+// Styles
 import './App.css'
+// Components 
+import { NavbarMobile } from './components/NavbarMobile'
+//
+
+// COMPONENT 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // DARK MODE 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+  }, [isDarkMode]);
+
+  // navBar status 
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <NavbarMobile isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} toggleTheme={toggleTheme}/>
+      
+    </div>
   )
 }
 
