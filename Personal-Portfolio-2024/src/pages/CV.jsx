a2d53e44b1c8afbfc4d7b0e4fd29affdabb6defb
Gucci Gangnam <diviner02hive@icloud.com>
2025-07-14 14:14:48 +0100

    Gemini Response Error Message Updated

--- Personal-Portfolio-2024/src/pages/CV.jsx
+++ Personal-Portfolio-2024/src/pages/CV.jsx
@@ -419,7 +419,7 @@ export const CV = () => {
                             )}
                             {/* IF ERROR IN RESPONSE then say sorry */}
                             {geminiError && (
-                                <> I'm really sorry.  There's been an issue with the response</>
+                                <> I'm really sorry.  This LLM isn;t a fan of that question. Try re-phrasing the question.</>
                             )}
 
 
